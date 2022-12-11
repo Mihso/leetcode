ab715557cafb1b635823c03d0f1f641f27e14f64
Mihso <75351957+Mihso@users.noreply.github.com>
2022-12-11 00:34:01 -0800

    Time: 241 ms (5.32%), Space: 13.6 MB (13.24%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        
+        start = 0
+        
+        while start < len(nums)-1:
+            for n in range(0, len(nums)-start - 1):
+                if nums[n] > nums[n + 1]:
+                    nums[n],nums[n+1] = nums[n+1], nums[n]
+            print(nums)
+            start += 1
