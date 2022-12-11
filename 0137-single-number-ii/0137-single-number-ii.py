7c962dc6e6e8f10581ec14561071a69e54248bbb
Mihso <75351957+Mihso@users.noreply.github.com>
2022-12-11 15:42:03 -0800

    Time: 112 ms (33.64%), Space: 15.7 MB (27.57%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        diction = {}
+        
+        for n in nums:
+            if n not in diction:
+                diction[n] = 1
+            else:
+                diction[n] += 1
+        for d in diction:
+            if diction[d] == 1:
+                return d
+        
