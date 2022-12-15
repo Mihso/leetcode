44569830078c86029907bca3e4494a97415f0709
Mihso <75351957+Mihso@users.noreply.github.com>
2022-12-15 03:27:11 -0800

    Time: 239 ms (60.00%), Space: 17.3 MB (8.75%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -9,7 +9,6 @@ class Solution(object):
         while i**2 <= c:
             values.add(i**2)
             i += 1
-        print(values)
         for v in values:
             check = c - v
             if check in values:
