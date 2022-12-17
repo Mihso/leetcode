10030da4146facfec29b7f791f8e72806a1147da
Mihso <75351957+Mihso@users.noreply.github.com>
2022-12-16 23:30:34 -0800

    Time: 16 ms (93.41%), Space: 13.6 MB (19.78%) - LeetHub

---
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def uncommonFromSentences(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: List[str]
+        """
+        list1 = s1.split(" ")
+        list2 = s2.split(" ")
+        print(list1, list2)
+        set1 = {}
+        set2 = {}
+        result = []
+        for l in list1:
+            if l not in set1:
+                set1[l] = 1
+            else:
+                set1[l] += 1
+        for l in list2:
+            if l not in set2:
+                set2[l] = 1
+            else:
+                set2[l] += 1
+        for s in set1:
+            if s not in set2 and set1[s] == 1:
+                result.append(s)
+        for s in set2:
+            if s not in set1 and set2[s] == 1:
+                result.append(s)
+        return result
+        
