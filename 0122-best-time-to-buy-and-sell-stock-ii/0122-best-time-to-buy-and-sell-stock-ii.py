0cf5601b765a6e30a971c491dcb016fe9db4381b
Matthew Oshimo <75351957+Mihso@users.noreply.github.com>
2022-12-29 17:42:44 -0800

    Time: 76 ms (56.67%), Space: 14.5 MB (69.76%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        start = 0
+        result = 0
+        for n in range(1,len(prices)):
+            if prices[n-1] > prices[n]:
+                result += prices[n-1] - prices[start]
+                start = n
+            elif n == len(prices)-1:
+                result += prices[n] - prices[start]
+            print(result)
+        return result
+                
