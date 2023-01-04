19ccbc6da32ac9457ac32c5e95001d7b6a612a22
Matthew Oshimo <75351957+Mihso@users.noreply.github.com>
2023-01-03 21:46:28 -0800

    Time: 30 ms (36.44%), Space: 13.5 MB (46.68%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        result = []
+        up = 0
+        down = len(matrix) - 1
+        left = 0
+        right = len(matrix[0]) - 1
+        while up <= down:
+            for row in range(left, right + 1):
+                result.append(matrix[up][row])
+            up += 1
+            if left <= right:
+                for col in range(up, down + 1):
+                    result.append(matrix[col][right])
+                right -= 1
+            if up <= down:
+                for row in range(right, left -1, -1):
+                    result.append(matrix[down][row])
+                down -= 1
+                if left <= right:
+                    for col in range(down, up -1, -1):
+                        result.append(matrix[col][left])
+                    left += 1
+            print result
+        return result
