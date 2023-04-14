30d084def29dea9dfe2bece6056823c582d8dfa8
Matthew Oshimo <75351957+Mihso@users.noreply.github.com>
2023-04-14 00:03:26 -0700

    Time: 2007 ms (25.44%), Space: 107.2 MB (5.20%) - LeetHub

---
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def longestPalindromeSubseq(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        diction = {}
+        def palin(left, right):
+            answer = 0
+            if (left,right) in diction:
+                return diction[(left,right)]
+            if left == right:
+                return 1
+            if left > right:
+                return 0
+            if left < right:
+                if s[left] == s[right]:
+                    diction[(left,right)] = 2 + palin(left + 1, right - 1)
+                else:
+                    diction[(left,right)] = max(palin(left + 1, right), palin(left, right - 1))
+            return diction[(left,right)]
+            
+        
+        result = palin(0,len(s)-1)
+        
+        return result
