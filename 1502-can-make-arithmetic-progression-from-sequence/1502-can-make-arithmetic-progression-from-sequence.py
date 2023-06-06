1c125f1bf8c7872ecd464cfa10c2fcbce45085b2
Matthew Oshimo <75351957+Mihso@users.noreply.github.com>
2023-06-05 23:43:02 -0700

    Time: 34 ms (8.57%), Space: 13.5 MB (31.11%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.py
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.py
@@ -5,7 +5,6 @@ class Solution(object):
         :rtype: bool
         """
         arr.sort()
-        print(arr)
         diff = arr[1]-arr[0]
         for i in range(len(arr)-1):
             if arr[i+1] - arr[i] != diff:
