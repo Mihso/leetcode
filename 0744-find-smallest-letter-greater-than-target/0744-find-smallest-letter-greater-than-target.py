17b133afcef9d5a87fde4017773608f54ea7d95a
Matthew Oshimo <75351957+Mihso@users.noreply.github.com>
2023-06-08 22:28:00 -0700

    Time: 84 ms (41.19%), Space: 15.4 MB (40.91%) - LeetHub

---
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def nextGreatestLetter(self, letters, target):
+        """
+        :type letters: List[str]
+        :type target: str
+        :rtype: str
+        """
+        target = ord(target)
+        print(target)
+        
+        for l in letters:
+            if ord(l)> target:
+                return l
+        return letters[0]
