021aeea53aebdb513643bda176ad70b193333d6a
Matthew Oshimo <75351957+Mihso@users.noreply.github.com>
2023-06-29 03:12:07 -0700

    Time: 24 ms (16.10%), Space: 13.5 MB (34.17%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -5,14 +5,12 @@ class Solution(object):
         :type word2: str
         :rtype: str
         """
-        index = 0
-        result = ""
-        while index < len(word1) and index < len(word2):
-            result += word1[index]
-            result += word2[index]
-            index += 1
-        if index < len(word1):
-            result += word1[index:len(word1)]
-        elif index < len(word2):
-            result += word2[index:len(word2)]
-        return result
+        i1 = 0
+        i2 = 0
+        final = ""
+        while i1 < len(word1) and i2 < len(word2):
+            final += word1[i1]+word2[i2]
+            i1 +=1
+            i2+=1
+        
+        return final + word1[i1:len(word1)] + word2[i2:len(word2)]
