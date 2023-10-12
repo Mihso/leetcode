0932e68597fc977d4449e43dad463bb95b0b512c
Matthew Oshimo <75351957+Mihso@users.noreply.github.com>
2023-10-11 21:14:36 -0700

    Time: 259 ms (6.01%), Space: 44.5 MB (5.91%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,56 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<String> stacker = new Stack<String>();
+        
+        if(s.length() == 0 || s.length()%2 != 0){
+            return false;
+        }
+        
+        for(int i = 0; i < s.length(); i++){
+            String current = "";
+            current += s.charAt(i);
+            System.out.println(current);
+            if(current.equals(")"))
+            {
+                if(stacker.isEmpty()){
+                    return false;
+                }
+                String value = stacker.pop();
+                if(!value.equals("(")){
+                    return false;
+                }
+            }
+            else if(current.equals("}"))
+            {
+                if(stacker.isEmpty()){
+                    return false;
+                }
+                String value = stacker.pop();
+                if(!value.equals("{")){
+                    return false;
+                }
+            }
+            else if(current.equals("]"))
+            {
+                if(stacker.isEmpty()){
+                    return false;
+                }
+                String value = stacker.pop();
+                if(!value.equals("[")){
+                    return false;
+                }
+            }
+            else{
+                
+                stacker.push(current);
+                System.out.println(stacker);
+            }
+        }
+        if(stacker.isEmpty()){
+        return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
