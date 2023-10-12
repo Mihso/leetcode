b6e6bd326000b558812ca7021ee38f9e403babf4
Matthew Oshimo <75351957+Mihso@users.noreply.github.com>
2023-10-11 21:21:12 -0700

    Time: 268 ms (6.01%), Space: 44 MB (5.91%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,41 +1,40 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<String> stacker = new Stack<String>();
+        Stack<Character> stacker = new Stack<Character>();
         
         if(s.length() == 0 || s.length()%2 != 0){
             return false;
         }
         
         for(int i = 0; i < s.length(); i++){
-            String current = "";
-            current += s.charAt(i);
-            if(current.equals(")"))
+            Character current = s.charAt(i);
+            if(current.equals(')'))
             {
                 if(stacker.isEmpty()){
                     return false;
                 }
-                String value = stacker.pop();
-                if(!value.equals("(")){
+                Character value = stacker.pop();
+                if(!value.equals('(')){
                     return false;
                 }
             }
-            else if(current.equals("}"))
+            else if(current.equals('}'))
             {
                 if(stacker.isEmpty()){
                     return false;
                 }
-                String value = stacker.pop();
-                if(!value.equals("{")){
+                Character value = stacker.pop();
+                if(!value.equals('{')){
                     return false;
                 }
             }
-            else if(current.equals("]"))
+            else if(current.equals(']'))
             {
                 if(stacker.isEmpty()){
                     return false;
                 }
-                String value = stacker.pop();
-                if(!value.equals("[")){
+                Character value = stacker.pop();
+                if(!value.equals('[')){
                     return false;
                 }
             }
