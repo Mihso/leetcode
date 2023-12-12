caa5f9f6087169307ce6fedefc0953ab0b13e04b
Matthew Oshimo <75351957+Mihso@users.noreply.github.com>
2023-12-12 02:36:21 -0800

    Time: 1 ms (41.97%), Space: 41.1 MB (10.94%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,9 +1,6 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        System.out.println(s);
-        String[] newer = s.split(" ");
-        System.out.println(newer[newer.length - 1]);
-        
+        String[] newer = s.split(" ");   
         return newer[newer.length - 1].length();
     }
 }
