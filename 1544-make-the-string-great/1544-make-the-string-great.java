230bfe39a120c6f447a882f26c89c7c23da9ea09
Matthew Oshimo <75351957+Mihso@users.noreply.github.com>
2024-04-04 23:03:44 -0700

    Time: 14 ms (7.43%), Space: 42.8 MB (16.11%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -0,0 +1,40 @@
+class Solution {
+    public String makeGood(String s) {
+        boolean found = true;
+        
+        while(found){
+            found = false;
+            int i = 0;
+            while(i < s.length()-1){
+                
+                if(Character.toLowerCase(s.charAt(i)) == Character.toLowerCase(s.charAt(i+1))){
+                    if(!comp(s.charAt(i), s.charAt(i+1))){
+                        s = s.substring(0, i) + s.substring(i+2);
+                        found = true;
+                    }
+                    else{
+                        i++;
+                    }
+                }
+                else{
+                    i++;
+                }
+            }
+        System.out.println(s);
+        }
+        
+        return s;
+    }
+    
+    public boolean comp(char x, char y){
+        if(Character.isLowerCase(x) && Character.isLowerCase(y)){
+            return true;
+        }
+        else if(Character.isUpperCase(x) && Character.isUpperCase(y)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
