389903229c6a72c6745feb61935ec1b45e59ef69
Matthew Oshimo <75351957+Mihso@users.noreply.github.com>
2024-04-08 22:08:34 -0700

    Time: 0 ms (100.00%), Space: 41.2 MB (55.58%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -1,40 +1,53 @@
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
-        int tracker = k;
-        int counter = 0;
+//         int tracker = k;
+//         int counter = 0;
         
-        ArrayList<Integer> copy = new ArrayList<>();
+//         ArrayList<Integer> copy = new ArrayList<>();
         
-        for(int i: tickets){
-            copy.add(i);
-        }
+//         for(int i: tickets){
+//             copy.add(i);
+//         }
         
-        if(tickets.length > 1){
+//         if(tickets.length > 1){
         
-        while(copy.get(tracker) > 0){
-            int val = copy.get(0);
-            if(val > 0){
-                val -= 1;
-                copy.set(0, val);
+//         while(copy.get(tracker) > 0){
+//             int val = copy.get(0);
+//             if(val > 0){
+//                 val -= 1;
+//                 copy.set(0, val);
                 
                 
-            counter += 1;
-            }
+//             counter += 1;
+//             }
             
-            copy.remove(0);
-            copy.add(val);
+//             copy.remove(0);
+//             copy.add(val);
                 
-            tracker -=1;
-            if(tracker < 0){
-                tracker = tickets.length-1;
-            }
+//             tracker -=1;
+//             if(tracker < 0){
+//                 tracker = tickets.length-1;
+//             }
             
-        }
-            return counter;
-        }
+//         }
+//             return counter;
+//         }
         
-        else{
-            return(tickets[0]);
+//         else{
+//             return(tickets[0]);
+//         }
+        
+        
+        int total = 0;
+        
+        for(int i =0; i< tickets.length; i++){
+            if(i <= k){
+                total += Math.min(tickets[i], tickets[k]);
+            }
+            else{
+                total += Math.min(tickets[i], tickets[k] - 1);
+            }
         }
+        return total;
     }
 }
