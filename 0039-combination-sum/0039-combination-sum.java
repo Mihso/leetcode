bf53f00fca87bed5bbdacb7e123654a280d76374
Matthew Oshimo <75351957+Mihso@users.noreply.github.com>
2024-04-10 11:35:26 -0700

    Time: 127 ms (5.08%), Space: 45.2 MB (5.50%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,28 @@
+class Solution {
+    ArrayList<List<Integer>> answer = new ArrayList<>();
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<Integer> empty = new ArrayList<>();
+        checker(candidates, target, empty);
+        return answer;
+    }
+    
+    public void checker(int[] candidates, int target, List<Integer> current){
+        if(target == 0){
+            List<Integer> copy = new ArrayList<>();
+            for(int i: current){
+                copy.add(i);
+            }
+            Collections.sort(copy);
+            if(!answer.contains(copy)){
+                answer.add(copy);
+            }
+        }
+        else if(target > 0){
+            for(int c: candidates){
+                current.add(c);
+                checker(candidates, target - c, current);
+                current.remove(current.size()-1);
+            }
+        }
+    }
+}
