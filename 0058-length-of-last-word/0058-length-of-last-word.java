31dc87dc957a7eb4f95245c85525733b4224e8e0
Matthew Oshimo <75351957+Mihso@users.noreply.github.com>
2024-04-10 00:18:01 -0700

    Time: 0 ms (100.00%), Space: 41.5 MB (73.28%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,6 +1,16 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] newer = s.split(" ");   
-        return newer[newer.length - 1].length();
+        String[] words = s.split(" ");
+        int index = words.length - 1;
+        
+        while(index >= 0){
+            if(words[index].length() > 0){
+                return words[index].length();
+            }
+            else{
+                index -= 1;
+            }
+        }
+        return 0;
     }
 }
