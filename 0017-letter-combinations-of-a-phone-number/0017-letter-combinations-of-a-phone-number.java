96e1385706e2938552d00f72d2d4072645125f44
Matthew Oshimo <75351957+Mihso@users.noreply.github.com>
2024-04-13 02:12:19 -0700

    Time: 4 ms (45.93%), Space: 42.3 MB (17.32%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,33 @@
+class Solution {
+    List<String> answer = new ArrayList<>();
+    List<char[]> key = new ArrayList<>();
+    public List<String> letterCombinations(String digits) {
+        key.add(new char[]{'a','b','c'});
+        key.add(new char[]{'d','e','f'});
+        key.add(new char[]{'g','h','i'});
+        key.add(new char[]{'j','k','l'});
+        key.add(new char[]{'m','n','o'});
+        key.add(new char[]{'p','q','r','s'});
+        key.add(new char[]{'t','u','v'});
+        key.add(new char[]{'w','x','y','z'});
+        String stringer = "";
+        if(digits.length() == 0){
+            return answer;
+        }
+        combo(stringer, digits);
+        return answer;
+    }
+    
+    public void combo(String current, String getter){
+        if(getter.length() == 0 && current.length() > 0){
+            answer.add(current);
+        }
+        else{
+            for(char unit: key.get(Character.getNumericValue(getter.charAt(0))-2)){      
+                String copy = current + unit;
+                combo(copy, getter.substring(1, getter.length()));
+                }
+            }
+        
+    }
+}
