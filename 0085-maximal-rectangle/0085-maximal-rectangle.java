c34b7c5ca6392f7a2f32f36ed629a3cf784e1f59
Matthew Oshimo <75351957+Mihso@users.noreply.github.com>
2024-04-12 20:50:12 -0700

    Time: 16 ms (54.59%), Space: 47.5 MB (8.57%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int maximalRectangle(char[][] matrix) {
+        int answer = 0;
+        Integer[] tracker = new Integer[matrix[0].length];
+        for(int i = 0; i < tracker.length; i++){
+            tracker[i] = 0;
+        }
+        for(char[] c: matrix){
+            for(int i = 0; i < c.length; i++){
+                if(c[i] == '1'){
+                    tracker[i] += 1;
+                }
+                else{
+                    tracker[i] = 0;
+                }
+            }
+            int maxarea = 0;
+            Stack<Integer> heights = new Stack<>();
+            
+            for(int h: tracker){
+                if(!heights.contains(h)){
+                    heights.add(h);
+                }
+            }
+            
+            for(int s: heights){
+                int track = 0;
+                for(int i : tracker){
+                    if(i < s){
+                        maxarea = Math.max(maxarea, track);
+                        track = 0;
+                    }
+                    else{
+                        track += s;
+                    }
+                }
+                maxarea = Math.max(maxarea, track);
+            }
+            answer = Math.max(answer, maxarea);
+            
+        }
+        
+        return answer;
+        
+    }
+    
+}
