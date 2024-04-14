3e6e641eaad6a86af110776a6904b69d3e9624ed
Matthew Oshimo <75351957+Mihso@users.noreply.github.com>
2024-04-14 02:10:00 -0700

    Time: 1 ms (100.00%), Space: 42.4 MB (68.03%) - LeetHub

--- 0223-rectangle-area/0223-rectangle-area.java
+++ 0223-rectangle-area/0223-rectangle-area.java
@@ -1,13 +1,9 @@
 class Solution {
     public int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
         
-        if(ax1> bx2 || bx1 > ax2){
+        if(ax1> bx2 || bx1 > ax2 || ay1> by2 || by1 > ay2){
             return ((ax2 - ax1)*(ay2 - ay1)) + ((bx2-bx1)*(by2-by1)) ;
         }
-        if(ay1> by2 || by1 > ay2){
-            return((ax2 - ax1)*(ay2 - ay1)) + ((bx2-bx1)*(by2-by1)); 
-        }
-        
         return( ((ax2 - ax1)*(ay2 - ay1)) + ((bx2-bx1)*(by2-by1)) - ( (Math.min(bx2,ax2)-Math.max(bx1,ax1)) * (Math.min(by2, ay2)-Math.max(by1, ay1)) ));
     }
 }
