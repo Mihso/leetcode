8edaf64e786c5a43abe4576ef07876dd73af73ff
Matthew Oshimo <75351957+Mihso@users.noreply.github.com>
2024-04-17 23:30:32 -0700

    Time: 5 ms (6.23%), Space: 42.2 MB (6.55%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int[] checker = new int[obstacleGrid[0].length];
+        Boolean found = false;
+        for(int i = 0; i < obstacleGrid[0].length; i++){
+            if(obstacleGrid[0][i] == 1 && !found){
+                found = true;
+                checker[i] = 0;
+            }
+            else if(!found){
+                checker[i] = 1;
+            }
+            else{
+                checker[i] = 0;
+            }
+        }
+        
+        for(int i = 1; i< obstacleGrid.length; i++){
+            if(obstacleGrid[i][0] == 1){
+                checker[0] = 0;
+            }
+            else{
+                ;
+            }
+            for(int x = 1; x < obstacleGrid[i].length; x++){
+                if(obstacleGrid[i][x] == 1){
+                    checker[x] = 0;
+                }
+                else{
+                    checker[x] += checker[x-1];
+                }
+            }
+            System.out.println(Arrays.toString(checker));
+        }
+        return checker[checker.length - 1];
+        
+    }
+}
