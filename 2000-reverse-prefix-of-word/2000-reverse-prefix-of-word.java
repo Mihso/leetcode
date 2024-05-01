7bdb500067d90105c65df4063bdfd33b79149cca
Matthew Oshimo <75351957+Mihso@users.noreply.github.com>
2024-04-30 17:10:52 -0700

    Time: 5 ms (19.70%), Space: 41.2 MB (91.13%) - LeetHub

---
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String reversePrefix(String word, char ch) {
+        int x = -1;
+        String answer = "";
+        int i = 0;
+        while(x == -1 && i < word.length()){
+            if(word.charAt(i) == ch){
+                x = i;
+            }
+            
+            if(x != -1){
+                for(int b = x; b >= 0; b--){
+                    answer += word.charAt(b);
+                }
+            }
+            i++;
+        }
+        
+        if(x == -1){
+            return word;
+        }
+        
+        answer += word.substring(x+1, word.length());
+            
+        return answer;
+        
+    }
+}
