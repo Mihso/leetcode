1929e903e658224ce502c7c539ad61ea02579378
Matthew Oshimo <75351957+Mihso@users.noreply.github.com>
2024-05-05 23:48:53 -0700

    Time: 36 ms (9.55%), Space: 49.2 MB (6.48%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,List<String>> mapper = new HashMap<>();
+        
+        for(String s: strs){
+            int[] freq = new int[26];
+            Arrays.fill(freq, 0);
+            for(int i = 0; i < s.length(); i++){
+                freq[s.charAt(i)-'a']++;
+            }
+            String current = makeKey(freq);
+            
+            if(!mapper.containsKey(current)){
+                mapper.put(current, new ArrayList<String>());
+            }
+            
+            mapper.get(current).add(s);
+            
+        }
+        List<List<String>> answer = new ArrayList<>();
+        
+        answer.addAll(mapper.values());
+        
+        return answer;
+        
+    }
+    
+    public String makeKey(int[] freq){
+        String answer = "";
+        for(int i: freq){
+            answer += "-" + i;
+        }
+        
+        return answer;
+    }
+}
