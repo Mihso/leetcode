9b1eb8c962d7678b592a426ed5e69e44ae6568a8
Matthew Oshimo <75351957+Mihso@users.noreply.github.com>
2024-05-07 17:16:13 -0700

    Time: 71 ms (12.28%), Space: 45.3 MB (92.42%) - LeetHub

---
+++ 0506-relative-ranks/0506-relative-ranks.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String[] findRelativeRanks(int[] score) {
+        String[] answer = new String[score.length];
+        int i = 0;
+        while(i < answer.length){
+            int maximum = 0;
+            for(int y = 0; y < score.length; y++){
+                if(score[maximum] < score[y]){
+                    maximum = y;
+                }
+            }
+            if(i == 0){
+                answer[maximum] = "Gold Medal";
+            }
+            else if(i == 1){
+                answer[maximum] = "Silver Medal";
+            }
+            else if(i == 2){
+                answer[maximum] = "Bronze Medal";
+            }
+            else{
+                answer[maximum] = Integer.toString(i+1);
+            }
+            
+            score[maximum] = -1;
+            i++;
+        }
+        return answer;
+    }
+}
