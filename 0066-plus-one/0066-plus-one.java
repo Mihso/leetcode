894194110366910f0f2302c070d6f292d45b13ec
Matthew Oshimo <75351957+Mihso@users.noreply.github.com>
2024-05-19 22:25:17 -0700

    Time: 0 ms (100.00%), Space: 42 MB (14.71%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,8 +1,7 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int i = digits.length - 1;
         int carry = 1;
-        while(i >= 0){
+        for(int i = digits.length - 1; i >= 0; i--){
             digits[i] += carry;
             if(digits[i] == 10){
                 digits[i] = 0;
@@ -11,7 +10,6 @@ class Solution {
             else{
                 carry = 0;
             }
-            i--;
         }
         
         if(carry == 1){
