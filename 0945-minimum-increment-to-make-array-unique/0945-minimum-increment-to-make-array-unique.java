e1b05a68cda7df296d12b7f2a8b75d5e0c4cdea2
Matthew Oshimo <75351957+Mihso@users.noreply.github.com>
2024-06-13 17:29:57 -0700

    Time: 37 ms (72.13%), Space: 59.4 MB (84.92%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.java
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.java
@@ -4,9 +4,8 @@ class Solution {
         Arrays.sort(nums);
         for(int i = 1; i < nums.length; i++){
             while(nums[i] <= nums[i-1]){
-                int dif = (nums[i-1] + 1) - nums[i];
+                counter += (nums[i-1] + 1) - nums[i];
                 nums[i] = nums[i - 1]+ 1;
-                counter += dif;
             }
         }
         
