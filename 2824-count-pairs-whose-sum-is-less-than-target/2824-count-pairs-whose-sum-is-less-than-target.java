f1a6126600106a05d3c91031602e00a9af144b8f
Matthew Oshimo <75351957+Mihso@users.noreply.github.com>
2024-06-13 17:34:18 -0700

    Time: 3 ms (33.92%), Space: 42.9 MB (5.68%) - LeetHub

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.java
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.java
@@ -3,8 +3,7 @@ class Solution {
         int counter = 0;
         
         while(!nums.isEmpty()){
-            int first = nums.get(0);
-            nums.remove(0);
+            int first = nums.remove(0);
             for(int i: nums){
                 if(first + i < target){
                     counter += 1;
