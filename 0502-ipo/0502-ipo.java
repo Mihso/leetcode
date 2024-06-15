5987c546676c349717bdf517eefac4888fc3216e
Matthew Oshimo <75351957+Mihso@users.noreply.github.com>
2024-06-14 22:56:54 -0700

    Time: 667 ms (5.06%), Space: 61.6 MB (30.16%) - LeetHub

---
+++ 0502-ipo/0502-ipo.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        ArrayList<int[]> pairs = new ArrayList<>(); 
+        int counter = 0;
+        
+        for(int i = 0; i < profits.length; i++){
+            pairs.add(new int[] {profits[i], capital[i]});
+        }
+        Collections.sort(pairs,(int[] a, int[] b) -> b[0] - a[0]);
+        
+        PriorityQueue<int[]> q = new PriorityQueue<>((int[]a, int[] b) -> b[0] - a[0]);
+        
+        while(counter < k){
+            int index = 0;
+            while(index < pairs.size()){
+                if(pairs.get(index)[1] <= w){
+                    q.add(pairs.get(index));
+                    pairs.remove(index);
+                }
+                else{
+                    index++;
+                }
+            }
+            if(q.isEmpty()){
+                break;
+            }
+            
+            else{
+                w += q.poll()[0];
+            }
+            
+            counter++;
+            
+        }
+        return w;
+        
+    }
+}
