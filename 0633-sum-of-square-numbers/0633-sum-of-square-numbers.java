445e8bda4fd00c2d27a3923785d62b52396675fd
Matthew Oshimo <75351957+Mihso@users.noreply.github.com>
2024-06-16 17:37:36 -0700

    Time: 9 ms (9.98%), Space: 40.6 MB (14.02%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
@@ -1,8 +1,6 @@
 class Solution {
     public boolean judgeSquareSum(int c) {
-        List<Integer> vals = new ArrayList<>();
-        int count = 0;
-        
+        int count = 0; 
         while(Math.pow(count, 2) <= c){
             int sq = (int)Math.sqrt(c - ((int)Math.pow(count, 2)));
             if(Math.sqrt(c-((int)Math.pow(count, 2))) == sq ){
