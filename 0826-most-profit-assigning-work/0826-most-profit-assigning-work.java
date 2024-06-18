d133be042235719e3ebdba1b2ed3d7b89c5fd9ed
Matthew Oshimo <75351957+Mihso@users.noreply.github.com>
2024-06-17 17:22:41 -0700

    Time: 1338 ms (5.03%), Space: 45.7 MB (39.31%) - LeetHub

---
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
+        int[] out = new int[worker.length];
+        
+        for(int j = 0; j < worker.length; j++){
+            out[j] = value(worker[j],difficulty, profit);
+        }
+        
+        int answer = 0;
+        for(int i: out){
+            answer += i;
+        }
+        return answer;
+    }
+    
+    public int value(int d, int[] difficulty, int[] profit){
+        int answer = 0;
+        for(int i = difficulty.length - 1; i >= 0; i--){
+            if(difficulty[i] <= d){
+                answer = Math.max(answer, profit[i]);
+            }
+        }
+        return answer;
+    }
+}
