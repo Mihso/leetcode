1158bae90e4745592ba4d9a3d6516f66f6429708
Matthew Oshimo <75351957+Mihso@users.noreply.github.com>
2024-06-29 22:53:18 -0700

    Time: 11 ms (35.18%), Space: 45.1 MB (18.30%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,9 +1,7 @@
 class Solution {
     public String reverseWords(String s) {
         String[] words = s.split(" ");
-        
-        System.out.println(Arrays.toString(words));
-        
+                
         String answer = "";
         
         for(String x: words){
