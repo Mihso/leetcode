0d37e85adb721b4a4056dfe0babc47e202aeb0f9
Matthew Oshimo <75351957+Mihso@users.noreply.github.com>
2024-08-29 00:07:52 -0700

    Time: 0 ms (100.00%), Space: 41.9 MB (90.62%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,30 +1,18 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int lengther = matrix.length;
-        for(int y = 0; y < matrix.length; y++){
-            int top = 0;
-            int bottom = lengther - 1;
-            while(top < bottom){
-                int store = matrix[top][y];
-                
-                matrix[top][y] = matrix[bottom][y];
-                
-                matrix[bottom][y] = store;
-                
-                top++;
-                bottom--;
+        for(int x = 0; x < matrix.length; x++){
+            for(int y = 0; y < x; y++){
+                int value = matrix[x][y];
+                matrix[x][y] = matrix[y][x];
+                matrix[y][x] = value;
             }
         }
-        int index = 0;
-        while(index < lengther){
-            for(int x = index; x < lengther; x++){
-                int store = matrix[x][index];
-                matrix[x][index] = matrix[index][x];
-                matrix[index][x] = store;
+        for(int x = 0; x < matrix.length; x++){
+            for(int y = 0; y < matrix[x].length / 2; y++){
+                int value = matrix[x][y];
+                matrix[x][y] = matrix[x][matrix.length - 1 - y];
+                matrix[x][matrix.length - 1 - y] = value;
             }
-            
-            index++;
         }
-        
     }
 }
