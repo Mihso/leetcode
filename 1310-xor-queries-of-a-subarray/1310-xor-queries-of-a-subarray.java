0be48ebac6e97f117672ec6d7917e9f7efd11f4b
Matthew Oshimo <75351957+Mihso@users.noreply.github.com>
2024-09-13 12:00:21 -0700

    Time: 2 ms (100.00%), Space: 58.5 MB (24.40%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.java
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.java
@@ -2,9 +2,17 @@ class Solution {
     public int[] xorQueries(int[] arr, int[][] queries) {
         int[] answer = new int[queries.length];
         
+        for(int i = 1; i < arr.length; i++){
+            arr[i] ^= arr[i - 1];
+        }
+        
         for(int i = 0; i < queries.length; i++){
-            for(int y = queries[i][0]; y <= queries[i][1]; y++)
-            answer[i] ^= arr[y];
+            if(queries[i][0] > 0){
+                answer[i] = arr[queries[i][1]] ^ arr[queries[i][0] - 1];
+            }
+            else{
+                answer[i] = arr[queries[i][1]];
+            }
         }
         
         return answer;
