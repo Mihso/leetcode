e19c106b12e5cbb9d710f58dae1b8860ee4a32d4
Matthew Oshimo <75351957+Mihso@users.noreply.github.com>
2024-11-26 01:34:07 -0800

    Time: 7 ms (14.29%), Space: 46.3 MB (5.15%) - LeetHub

--- 2924-find-champion-ii/2924-find-champion-ii.java
+++ 2924-find-champion-ii/2924-find-champion-ii.java
@@ -14,30 +14,35 @@ class Solution {
         
         for(int i: incoming.keySet()){
             if(incoming.get(i).size() == 0){
-                queue.add(i);
+                if(champion == n){
+                    champion = i;
+                }
+                else{
+                    return -1;
+                }
             }
         }
         
-        while(!queue.isEmpty()){
-            int lengther = queue.size();
-            for(int i = 0; i < lengther; i++){
-                int current = queue.remove();
-                visited.add(current);
-                if(incoming.get(current).size() == 0 && champion == n){
-                    champion = current;
-                }
-                else if(incoming.get(current).size() == 0){
-                    return -1;
-                }
-                else{
-                    for(int neighbor: incoming.get(current)){
-                        if(!visited.contains(neighbor)){
-                            queue.add(neighbor);
-                        }
-                    }
-                }
-            }
-        }
+        // while(!queue.isEmpty()){
+        //     int lengther = queue.size();
+        //     for(int i = 0; i < lengther; i++){
+        //         int current = queue.remove();
+        //         visited.add(current);
+        //         if(incoming.get(current).size() == 0 && champion == n){
+        //             champion = current;
+        //         }
+        //         else if(incoming.get(current).size() == 0){
+        //             return -1;
+        //         }
+        //         else{
+        //             for(int neighbor: incoming.get(current)){
+        //                 if(!visited.contains(neighbor)){
+        //                     queue.add(neighbor);
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
         if(champion == n){
             return -1;
         }
